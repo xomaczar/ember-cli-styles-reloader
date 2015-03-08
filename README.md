6c35c9e1daa3bc9ff981ad36ec2090c76028863e
Andrey Khomenko <xomaczar@gmail.com>
2015-03-08 00:03:04 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # Ember-cli-styles-reloader
-
-This README outlines the details of collaborating on this Ember addon.
+Reloads changed styles (css|scss|sass|less|styl) without reloading the entire ember-cli app.
 
 ## Installation
 
-* `git clone` this repository
-* `npm install`
-* `bower install`
+### As an Ember CLI addon
+
+In your ember-cli project run
+
+```bash
+npm install --save-dev ember-cli-styles-reloader
+```
 
 ## Running
 
 * `ember server`
-* Visit your app at http://localhost:4200.
+* Visit your app at http://localhost:4200
 
 ## Running Tests
 
--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 /* jshint node: true */
 'use strict';
 
+/* default reload css extensions */
+var styleExtensions = ['css', 'scss', 'sass', 'less', 'styl'];
+var reloadCssPattern = new RegExp('.(' + styleExtensions.join('|') + ')$' );
+var noop = function(){};
+
+function LiveStyleReloader(options){
+
+    var options = options,
+        ui = options.ui,
+        http = null,
+        liveReloadHostname = ['http://', options.host, ':', options.liveReloadPort].join(''),
+        reloadCssPattern = options.reloadCssPattern;
+
+    var fileDidChange = function(results){
+        var filePath = results.filePath || '';
+
+        if (filePath.match(reloadCssPattern)){
+            http.get(liveReloadHostname + '/changed?files=*.css')
+                .on('error', noop);
+        }
+    };
+
+    this.startObserving = function(watcher){
+        if (options.liveReload){
+            ui.writeLine('StyleReloader refreshes ' + styleExtensions.join('|'));
+            http = require('http');
+            watcher.on('change', fileDidChange.bind(this));
+        }
+    };
+};
+
+
 module.exports = {
-  name: 'ember-cli-styles-reloader'
+  name: 'ember-cli-styles-reloader',
+
+  serverMiddleware: function(options){
+    var options = options.options;
+
+   // Tell ember-cli to ignore files that match reloadCssPattern
+    options.project.liveReloadFilterPatterns.push(reloadCssPattern);
+    options.reloadCssPattern = reloadCssPattern
+
+    var lsReloader = new LiveStyleReloader(options);
+    lsReloader.startObserving(options.watcher);
+  },
+
 };
